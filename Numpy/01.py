944425cf0a9618d7841c146841b1b902caf09f8f
Aditya yadav <adityayadaw42@gmail.com>
2025-11-22 12:59:13 +0530

    Numpy

--- Numpy/01.py
+++ Numpy/01.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 # a=[1,2,3]
 # print(type(a))
@@ -62,3 +63,45 @@ print(np.random.rand(2,3))
 #Random int array (it will print random number of shape 2,3 number between 1 to 10)
 
 print(np.random.randint(1,10,(3,2)))
+
+# slicing
+
+a=np.array([1,2,3,4,5,6])
+print(a[0])
+
+# print(a[0:5:2]) it will print from 0 to 5th index 5th(exclusive) by 2 step
+
+print(a[-3:])
+
+# Array reshaping and flattening
+arr1=np.array([1,2,3,4,5,6])
+print(arr1.reshape((2,3)))
+
+# Array flattening 
+
+arr2=np.array([[1,2,3],[4,5,6]])
+print(arr2.flatten())
+
+# stacking 
+
+x=np.array([1,2,4])
+y=np.array([5,6,7])
+
+print(np.hstack((x,y)))
+print(np.vstack((x,y)))
+
+# Mathemetical operations
+p=np.array([10,20,30])
+q=np.array([40,50,60])
+
+print(p+q)
+print(p-q)
+print(p*q)
+print(p/q)
+
+print("Mean of p is ",np.mean(p))
+print("median of p is ",np.median(p))
+print("standard deviation of p is ",np.std(p))
+print("Minimum value in p is",np.min(p))
+print("Maximum value in p is",np.max(p))
+print("sum of p is ",np.sum(p))
